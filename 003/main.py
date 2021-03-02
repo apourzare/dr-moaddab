3f8e6ef9aaf94a45c405f15187a188bd360ef2c2
Amin Pourzare <apourzare@gmail.com>
2021-03-01 19:30:59 -0800

    Complex example if

--- 003/main.py
+++ 003/main.py
@@ -5,3 +5,14 @@
 #     print("Mr", "Amin Pourzare")
 # else:
 #     print("Amin Pourzare")
+
+my_age = 6
+
+if my_age > 18:
+    print("You are adult")
+elif my_age > 10:
+    print("You are yong")
+elif my_age > 5:
+    print("You are children")
+else:
+    print("You are tiny")
